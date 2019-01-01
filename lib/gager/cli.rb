fbef5ef55410bed4b1a11a1fbf5d0e7aca465e2a
Jonas Kay <jonaskay@iki.fi>
2019-01-01 12:48:53 +0200

    Initial commit

---
+++ lib/gager/cli.rb
@@ -0,0 +1,8 @@
+require "gager/cli/version"
+
+module Gager
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/gager/cli/version.rb
@@ -0,0 +1,5 @@
+module Gager
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
